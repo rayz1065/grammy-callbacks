25d0ceff1b37a623a68961aa3e44995743dcc54b
rayz1065 <rayz1065@gmail.com>
2025-03-03 14:30:22 +0100

    Add method to migrate callback schemas

--- src/callbacks-bag.ts
+++ src/callbacks-bag.ts
@@ -29,6 +29,10 @@ export type CallbacksBagFlavor = {
   matchedCallback?: unknown;
 };
 
+type CallbacksBagCodec<C extends Context, T extends CallbackSchema> =
+  & Omit<CallbackDataCodec<T>, "filter">
+  & { composer: Composer<CallbackBagContext<C, T>> };
+
 /**
  * A container for multiple callbacks. The `register` function allows adding
  * more callback queries to the listened ones.
@@ -47,9 +51,48 @@ export type CallbacksBag<C extends Context> = {
     prefix: string,
     schema: T,
     ...middleware: Middleware<CallbackBagContext<C, T>>[]
-  ) => Omit<CallbackDataCodec<T>, "filter"> & {
-    composer: Composer<CallbackBagContext<C, T>>;
-  };
+  ) => CallbacksBagCodec<C, T>;
+  /**
+   * Utility function to migrate a callback query from an old schema to a new
+   * schema.
+   * When updating the schema of a callback query you _must_ change the prefix
+   * to ensure that existing buttons in chats don't fail validation (or worse,
+   * succeed with incorrect data).
+   * This function helps you maintain backwards compatibility, by offering a way
+   * to migrate the data from the old schema to the new schema.
+   *
+   * **NOTE**:
+   * - It's your responsibility to ensure that the data conforms to the new
+   * schema, you can ensure this by using Typescript;
+   * - **Only pass codec's generated from this bag**.
+   *
+   * Example:
+   * ```ts
+   * const counterCb = callbacks.register(
+   *   "count2",
+   *   { value: "number", increment: "number" },
+   *   async (ctx) => {},
+   * );
+   *
+   * callbacks.migrate({
+   *   old: { prefix: "count", schema: { value: "number" } },
+   *   new: counterCb,
+   *   adapter: (ctx) => {
+   *     const { value } = ctx.matchedCallback;
+   *     return { increment: 1, value };
+   *   },
+   * });
+   * ```
+   */
+  migrate: <T extends CallbackSchema, T2 extends CallbackSchema>(
+    migrationInfo: {
+      old: { prefix: string; schema: T };
+      new: CallbacksBagCodec<C, T2>;
+      adapter: NoInfer<
+        (ctx: CallbackBagContext<C, T>) => InferCallbackSchema<T2>
+      >;
+    },
+  ) => void;
 };
 
 /**
@@ -118,6 +161,39 @@ export function createCallbacksBag<C extends Context & CallbacksBagFlavor>(
   );
 
   return {
+    migrate: (migrationInfo) => {
+      const { old: { prefix, schema }, new: newCb, adapter } = migrationInfo;
+      if (usedPrefixes.has(prefix)) {
+        throw new Error(`Prefix ${prefix} already in use`);
+      }
+      usedPrefixes.add(prefix);
+      const callbackData = createCallbackData(prefix, schema);
+
+      const unpackSafe = (callback: MaybeNestedCallbackData) => {
+        callback = normalizeCallbackData(callback);
+        const [, childCallback] = splitWithTail(callback, sep, 2);
+        return callbackData.unpackSafe(childCallback);
+      };
+
+      composer
+        .callbackQuery(
+          new RegExp(
+            `^${escapeRegExp(commonPrefixSep)}${prefix}${escapedSep}.*$`,
+          ),
+        )
+        .lazy((ctx) => {
+          const res = unpackSafe(ctx.callbackQuery);
+          if (res.success) {
+            ctx.matchedCallback = res.data;
+            // deno-lint-ignore no-explicit-any
+            ctx.matchedCallback = adapter(ctx as any);
+            return newCb.composer as Middleware<C>;
+          } else if (res.reason === "VALIDATION_ERROR") {
+            return [onValidationError];
+          }
+          return [];
+        });
+    },
     register: (prefix, schema, ...middleware) => {
       if (usedPrefixes.has(prefix)) {
         throw new Error(`Prefix ${prefix} already in use`);
